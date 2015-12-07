9ebfd50ff7ee0a2dcc48129e8e8dc727b5ae4964
Dan Strawser <dstrawse@mit.edu>
2015-12-06 23:09:42 -0500

    update with dmn

--- DMNPureTheano3.py
+++ DMNPureTheano3.py
@@ -16,7 +16,7 @@ class DMNPureTheano3(object):
     # We take as input a string of "facts"
     def __init__(self, num_fact_hidden_units, number_word_classes, dimension_fact_embeddings, num_episode_hidden_units, max_number_of_facts_read):
 
-        self.X_train, self.mask_train, self.question_train, self.Y_train, self.X_test, self.mask_test, self.question_test, self.Y_test, word2idx, idx2word, dimension_fact_embeddings, max_queslen = self.process_data("embeddings")
+        self.X_train, self.mask_train, self.question_train, self.question_train_mask, self.Y_train, self.X_test, self.mask_test, self.question_test, self.question_test_mask, self.Y_test, word2idx, idx2word, dimension_fact_embeddings, max_queslen = self.process_data("embeddings")
 
         number_word_classes = max(idx2word.keys(), key=int)
         dimension_fact_embeddings = 5
@@ -30,8 +30,6 @@ class DMNPureTheano3(object):
         num_hidden_units_facts = num_hidden_units
         num_hidden_units_episodes = num_hidden_units_facts
         num_hidden_units_questions = num_hidden_units_episodes
-
-        question_idxs, question_mask, question = self.GRU_question(dimension_fact_embeddings, num_hidden_units_questions, num_hidden_units_episodes, max_queslen)
 
         # Size:  diemsnion of the embedding x the number of hidden units.  This is because it gets used for x_t * self.wx, where x_t is a word embedding
         self.wx = theano.shared(name='wx', value=0.2 * np.random.uniform(-1.0, 1.0, (de * cs, nh)).astype(theano.config.floatX))
@@ -62,6 +60,13 @@ class DMNPureTheano3(object):
         self.b_episode_to_answer = theano.shared(name='b_episode_to_answer', value=0.2 * np.random.uniform(-1.0, 1.0, number_word_classes).astype(theano.config.floatX))
         self.h0_episodes = theano.shared(name='h0_episodes', value=np.zeros(num_hidden_units_episodes, dtype=theano.config.floatX))
 
+        num_rows_z_dmn = 3
+        inner_dmn_dimension = 8
+        self.W_dmn_1 = theano.shared(name='W_dmn_1', value=0.2 * np.random.uniform(-1.0, 1.0, (inner_dmn_dimension , num_rows_z_dmn)).astype(theano.config.floatX))
+        self.W_dmn_2 = theano.shared(name='W_dmn_2', value=0.2 * np.random.uniform(-1.0, 1.0, (num_hidden_units_facts, inner_dmn_dimension)).astype(theano.config.floatX))
+
+        self.b_dmn_1 = theano.shared(name='b_dmn_1', value=0.2 * np.random.uniform(-1.0, 1.0, num_hidden_units_facts).astype(theano.config.floatX))
+        self.b_dmn_2 = theano.shared(name='b_dmn_2', value=0.2 * np.random.uniform(-1.0, 1.0, num_hidden_units_facts).astype(theano.config.floatX))
 
 
         self.params = [self.emb, self.w, self.b, self.W_fact_reset_gate_h, self.W_fact_reset_gate_x,
@@ -74,23 +79,28 @@ class DMNPureTheano3(object):
         x = self.emb[fact_idxs].reshape((fact_idxs.shape[0], de*cs)) # x basically represents the embeddings of the words IN the current sentence.  So it is shape
         y_sentence = T.iscalar('y_sentence')
 
-        def fact_gru_recursion(m_t, x_t, h_tm1):
+        question_idxs, question_mask, question = self.GRU_question(dimension_fact_embeddings, num_hidden_units_questions, num_hidden_units_episodes, max_queslen)
+
+        def fact_gru_recursion(mask_t, x_t, h_tm1, m_tm1):
 
             reset_gate_fact = T.nnet.sigmoid(T.dot(x_t, self.W_fact_reset_gate_x) + T.dot(h_tm1, self.W_fact_reset_gate_h))
             update_gate_fact = T.nnet.sigmoid(T.dot(x_t, self.W_fact_update_gate_x) + T.dot(h_tm1, self.W_fact_update_gate_h))
             hidden_update_in_fact = T.nnet.sigmoid(T.dot(x_t, self.W_fact_hidden_gate_x) + reset_gate_fact * T.dot(h_tm1, self.W_fact_hidden_gate_h))
             h_t = (1 - update_gate_fact) * h_tm1 + update_gate_fact * hidden_update_in_fact
 
-            #z_dmn = T.concatenate([question])
+            z_dmn = T.concatenate([x_t, m_tm1, question], axis=0)
+            G_dmn = T.nnet.sigmoid(T.dot(self.W_dmn_2, T.tanh(T.dot(self.W_dmn_1, z_dmn)) + self.b_dmn_1) + self.b_dmn_2)
 
-            h_t = m_t * h_t + (1 - m_t) * h_tm1
+            h_t = G_dmn * h_t + (1 - G_dmn) * h_tm1
+
+            h_t = mask_t * h_t + (1 - mask_t) * h_tm1
 
             s_t = T.nnet.softmax(T.dot(h_t, self.w) + self.b)
             return [h_t, s_t]
 
         def episode_gru_recursion(h_tm1):
 
-            [h, s_t], _ = theano.scan(fn=fact_gru_recursion, sequences=[fact_mask, x], outputs_info=[self.h0_facts, None], n_steps=x.shape[0])
+            [h, s_t], _ = theano.scan(fn=fact_gru_recursion, sequences=[fact_mask, x], outputs_info=[self.h0_facts, None], non_sequences=h_tm1, n_steps=x.shape[0])
             x_t = s_t[-1, 0, :]  # Note:  I believe this is (1x11)
 
             reset_gate_episode = T.nnet.sigmoid(T.dot(x_t, self.W_episode_reset_gate_x) + T.dot(h_tm1, self.W_episode_reset_gate_h))
@@ -144,16 +154,16 @@ class DMNPureTheano3(object):
 
             ll = 0
             for idx in range(len(self.X_train)):
-                x, m, q, y = self.X_train[idx], self.mask_train[idx], self.question_train[idx], self.Y_train[idx]
-                ll += self.sentence_train(x, m, y, lr)
+                x, m, q, mask_question, y = self.X_train[idx], self.mask_train[idx], self.question_train[idx], self.question_train_mask[idx], self.Y_train[idx]
+                ll += self.sentence_train(x, m, q, mask_question, y, lr)
 
             correct = 0
             total_tests = 0
 
             for idx in range(len(self.X_test)):
-                x, m, q, y = self.X_test[idx], self.mask_test[idx], self.question_test[idx], self.Y_test[idx]
+                x, m, q, mask_question, y = self.X_test[idx], self.mask_test[idx], self.question_test[idx], self.question_test_mask[idx], self.Y_test[idx]
 
-                predictions_test = self.classify(x, m)
+                predictions_test = self.classify(x, m, q, mask_question)
 
                 if idx == 30:
                     print(" prediction test: ", predictions_test)
@@ -203,47 +213,81 @@ class DMNPureTheano3(object):
         return question_idxs, question_mask, s[-1, 0, :]
 
 
-
     def process_data(self, type_of_embedding):
 
         filename_train = 'data/simple_dmn_theano/data_train_questioned.txt'
         filename_test = 'data/simple_dmn_theano/data_test_questioned.txt'
 
-        X_train, mask_train, Question_train, Y_train, X_test, mask_test, Question_test, Y_test, max_queslne = [], [], [], [], [], [], [], [], 0
+        X_train, mask_train, Question_train, Question_train_mask, Y_train, X_test, mask_test, Question_test, Question_test_mask, Y_test, max_queslen = [], [], [], [], [], [], [], [], [], [], 0
 
         cur_idx = 0
         word2idx = {}
         idx2word = {}
         
-        max_mask_len = 3
+        max_mask_len = 0
+        max_queslen = 0
 
         with open(filename_train, encoding='utf-8') as f:
-            cur_sentence = []                        
-            for idx, line in enumerate(f):
-                if "?" not in line:
-                    cur_sentence.append(line.strip())                   
+            cur_sentence = []
+            for line in f:
+                if "?" not in line and "@" not in line:
+                    cur_sentence.append(line.strip())
                 else:
-                    Y_train.append(line[2:].strip())
-                    Question_train.append("where")                    
-                    cur_mask = np.zeros(max_mask_len, dtype='int32')
-                    cur_mask[0:len(cur_sentence)] = 1
-                    mask_train.append(cur_mask)
-                    X_train.append(cur_sentence) # Since a question marks the end of the current sentence
+                    question_phrase = line[2:].split()
+                    cur_question = []
+
+                    if len(cur_sentence) > max_mask_len:
+                        max_mask_len = len(cur_sentence)
+                    if len(question_phrase) > max_queslen:
+                        max_queslen = len(question_phrase)
+                    for w in question_phrase:
+                        cur_question.append(w)
+
+                    Question_train.append((cur_question))
+                    X_train.append(cur_sentence)
+                    Y_train.append(next(f)[2:])
                     cur_sentence = []
 
         with open(filename_test, encoding='utf-8') as f:
-            cur_sentence = []                        
-            for idx, line in enumerate(f):
-                if "?" not in line:
-                    cur_sentence.append(line.strip())                   
+            cur_sentence = []
+            for line in f:
+                if "?" not in line and "@" not in line:
+                    cur_sentence.append(line.strip())
                 else:
-                    Y_test.append(line[2:].strip())  
-                    Question_test.append("where")                  
-                    cur_mask = np.zeros(max_mask_len, dtype='int32')
-                    cur_mask[0:len(cur_sentence)] = 1
-                    mask_test.append(cur_mask)
-                    X_test.append(cur_sentence) # Since a question marks the end of the current sentence
+                    question_phrase = line[2:].split()
+                    cur_question = []
+
+                    if len(cur_sentence) > max_mask_len:
+                        max_mask_len = len(cur_sentence)
+                    if len(question_phrase) > max_queslen:
+                        max_queslen = len(question_phrase)
+                    for w in question_phrase:
+                        cur_question.append(w)
+
+                    Question_test.append((cur_question))
+                    X_test.append(cur_sentence)
+                    Y_test.append(next(f)[2:])
                     cur_sentence = []
+
+        for l in X_train:
+            cur_mask = np.zeros(max_mask_len)
+            cur_mask[0:len(l)] = 1
+            mask_train.append(cur_mask)
+
+        for l in X_test:
+            cur_mask = np.zeros(max_mask_len)
+            cur_mask[0:len(l)] = 1
+            mask_test.append(cur_mask)
+
+        for l in Question_train:
+            cur_mask = np.zeros(max_queslen)
+            cur_mask[0:len(l)] = 1
+            Question_train_mask.append(cur_mask)
+
+        for l in Question_test:
+            cur_mask = np.zeros(max_queslen)
+            cur_mask[0:len(l)] = 1
+            Question_test_mask.append(cur_mask)
 
         for l in X_train + X_test:
             for s in l: 
@@ -252,20 +296,21 @@ class DMNPureTheano3(object):
                     idx2word[cur_idx] = s
                     cur_idx += 1
         
-        for y in Y_test + Y_train: 
-            if y not in word2idx: 
+        for y in Y_test + Y_train:
+            if y not in word2idx:
                 word2idx[y] = cur_idx
                 idx2word[cur_idx] = y
                 cur_idx += 1
-        
-        word2idx["where"] = cur_idx
-        idx2word[cur_idx] = "where" 
-        cur_idx += 1
 
-        assert(len(X_test) == len(Y_test))
+        for q in Question_train + Question_test:
+            for w in q:
+                if w not in word2idx:
+                    word2idx[w] = cur_idx
+                    idx2word[cur_idx] = w
+                    cur_idx += 1
+
         X_train_vec, Question_train_vec, Y_train_vec, X_test_vec, Question_test_vec, Y_test_vec = [], [], [], [], [], []
 
-        
         for s in X_train:
             cur_sentence = []
             for f in s:
@@ -304,23 +349,30 @@ class DMNPureTheano3(object):
                 new_vec[word2idx[y]] = 1
             Y_test_vec.append(new_vec)
 
-
         for q in Question_train:
-            if type_of_embedding == "embeddings":
-                new_vec = word2idx[q]
-            else:
-                new_vec = np.zeros((len(word2idx)))
-                new_vec[word2idx[q]] = 1
-            Question_train_vec.append(new_vec)
-       
+            cur_question = []
+            for w in q:
+                if type_of_embedding == "embeddings":
+                    new_vec = [word2idx[w]]
+                else:
+                    assert(1 == 2) # Not implemented
+                    #new_vec = np.zeros((len(word2idx)))
+                    #new_vec[word2idx[f]] = 1
+                cur_question.append(new_vec)
+            Question_train_vec.append(np.asmatrix(cur_question))
+
         for q in Question_test:
-            if type_of_embedding == "embeddings":
-                new_vec = word2idx[q]
-            else:
-                new_vec = np.zeros((len(word2idx)))
-                new_vec[word2idx[q]] = 1
-            Question_test_vec.append(new_vec)
+            cur_question = []
+            for w in q:
+                if type_of_embedding == "embeddings":
+                    new_vec = [word2idx[w]]
+                else:
+                    assert(1 == 2) # Not implemented
+                    #new_vec = np.zeros((len(word2idx)))
+                    #new_vec[word2idx[f]] = 1
+                cur_question.append(new_vec)
+            Question_test_vec.append(np.asmatrix(cur_question))
 
         assert(len(X_test_vec) == len(Y_test_vec))
             
-        return X_train_vec, mask_train, Question_train_vec, Y_train_vec, X_test_vec, mask_test, Question_test_vec, Y_test_vec, word2idx, idx2word, len(word2idx)
+        return X_train_vec, mask_train, Question_train_vec, Question_train_mask, Y_train_vec, X_test_vec, mask_test, Question_test_vec, Question_test_mask, Y_test_vec, word2idx, idx2word, len(word2idx), max_queslen
